f8b9850cd6ade4f0254d6010745ab978ad4241f0
chris <cdresher2010@gmail.com>
2021-12-27 13:00:18 -0500

    created deck.rb and deck_spec.rb

---
+++ spec/deck_spec.rb
@@ -0,0 +1,11 @@
+require 'deck'
+
+describe Deck do
+    describe '#initialize' do
+        it 'creates a deck array'
+
+        it 'has 52 cards in the deck'
+
+        it '4 of each suit per face value'
+    end
+end
