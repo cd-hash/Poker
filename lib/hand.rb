f25ebedbdfd609982915d6699efb5ba0ce5d6c8a
chris <cdresher2010@gmail.com>
2022-01-08 18:10:56 -0500

    almost done with hand logic need to finish calculate hand and then reorganize tests so the class loads properly.

--- lib/hand.rb
+++ lib/hand.rb
@@ -1,12 +1,30 @@
 class Hand
-    attr_reader :my_hand
+    attr_reader :my_hand, :hand_value
 
     def initialize(cards)
         @my_hand = cards
+        @hand_value = {}
+        #calculate_hand
     end
 
     def calculate_hand
-        
+        sorted_cards = sort_hand
+        matching_vals = contains_matching_numerical_value(sorted_cards)
+        matching_suits = contains_matching_suits(sorted_cards)
+        hand_hash = {}
+        if contains_sequence?(sorted_cards) #royal flush, straigh flush, straight req. sequence
+            if matching_suits.length != 1
+                hand_hash['straight'] = 5
+            elsif sorted_cards.sum { |card| card.numerical_value} == 55
+                hand_hash['royal flush'] = 10
+            else
+                hand_hash['straight flush']
+            end
+        else # four of a kind, full house, flush, 3 of a kind, 2 of a kind, 2 pair, pair, high card
+
+        end
+
+        @hand_value = hand_hash
     end
 
     def sort_hand
@@ -20,5 +38,29 @@ class Hand
             i += 1
         end
         return true
+    end
+
+    def contains_matching_numerical_value(sorted_hand)
+        matching_numbers = {}
+        sorted_hand.each do |card|
+            if matching_numbers.include?(card.numerical_value)
+                matching_numbers[card.numerical_value] += 1
+            else
+                matching_numbers[card.numerical_value] = 1
+            end
+        end
+        return matching_numbers
+    end
+
+    def contains_matching_suits(sorted_hand)
+        matching_suits = {}
+        sorted_hand.each do |card|
+            if matching_suits.include?(card.suit)
+                matching_suits[card.suit] += 1
+            else
+                matching_suits[card.suit] = 1
+            end
+        end
+        return matching_suits
     end
 end
--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -9,20 +9,22 @@ describe Hand do
         end
     end
     #create card doubles to test calculations
-    let(:king) { double('card', :numerical_value => 12, :face_value => 'spades') }
-    let(:queen) { double('card', :numerical_value => 11, :face_value => 'spades') }
-    let(:ten) { double('card', :numerical_value => 9, :face_value => 'spades') }
-    let(:five) { double('card', :numerical_value => 4, :face_value => 'spades') }
-    let(:seven) { double('card', :numerical_value => 6, :face_value => 'spades') }
+    let(:king) { double('card', :numerical_value => 12, :suit => 'spades', :face_value => 'king') }
+    let(:queen) { double('card', :numerical_value => 11, :suit => 'spades', :face_value => 'queen') }
+    let(:ten) { double('card', :numerical_value => 9, :suit => 'spades', :face_value => 'ten') }
+    let(:five) { double('card', :numerical_value => 4, :suit => 'spades', :face_value => 'five') }
+    let(:seven) { double('card', :numerical_value => 6, :suit => 'spades', :face_value => 'seven') }
 
     let(:sorted_hand) { [five, seven, ten, queen, king] }
 
     subject(:hand_with_cards) { Hand.new([king, five, ten, seven, queen]) }
 
     describe '#calculate_hand' do
-        it 'checks if there are any matching numerical values'
-        it 'checks if there are any matching suits'
-        it 'returns the classification and its associated ranking to the hand'
+        
+        
+        it 'returns the classification and its associated ranking to the hand' do
+            expect(hand_with_cards.hand_value).to eq( {'flush' => 6} )
+        end
     end
 
     describe '#sort_hand' do
@@ -37,6 +39,18 @@ describe Hand do
     describe '#contains_sequence?' do
         it 'checks if our hand contains cards in a numerical sequence' do
             expect(hand.contains_sequence?(sorted_hand)).to eq(false)
+        end
+    end
+
+    describe '#contains_matching_numerical_value' do
+        it 'checks if there are any matching numerical values' do
+            expect(hand.contains_matching_numerical_value(sorted_hand)).to eq({4 => 1, 6 => 1, 9 => 1, 11 => 1, 12 => 1})
+        end
+    end
+
+    describe '#contains_matching_suits' do
+        it 'checks if there are any matching suits' do 
+            expect(hand.contains_matching_suits(sorted_hand)).to eq({'spades' => 5})
         end
     end
 end
